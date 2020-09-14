c7bab252f7d540a9fcfbc0aa013f165402705705
Valentin Molina <valentinmolina83@gmail.com>
2020-09-13 19:46:21 -0700

    init app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from 'react';
+import "./App.css";
+import noTodosImage from "./images/relax-1.svg"
+import closeIcon from './images/close.svg';
 function App() {
+  const [showModal, setShowModal] = useState(false);
+  const [todos, setTodos] = useState([]);
+  const [todoTitle, setTodoTitle] = useState("");
+
+  const renderTodos = () => {
+    return todos.map((todo, i) => {
+      return (
+        <li className={todo.isComplete ? "complete" : ""} key={i}>
+          <label>{todo.title}</label>
+          <input onClick={() => { todos[i].isComplete = !todos[i].isComplete; setTodos([...todos])}} type="checkbox" value={todo.isComplete}></input>
+        </li>
+      );
+    })
+  }
+
+  const addTodo = (e) => {
+    e.preventDefault();
+    setTodos(todos => {
+      return todos.concat({
+        title: todoTitle,
+        isComplete: false
+      })
+    });
+
+    setShowModal(false);
+    setTodoTitle("");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Toodo</h1>
       </header>
+
+      <ul className={todos.length > 0 ? "todo-list-container" : "todo-list-container no-todos"}>
+        {
+          todos.length > 0 ? renderTodos() : (
+            <div>
+              <img src={noTodosImage} />
+              <h3>No Todos</h3>
+              <p>Take a moment to relax</p>
+            </div>
+          )
+        }
+      </ul>
+
+      <footer>
+        <button onClick={() => setShowModal(true)} className="add-todo-button">Add Todo</button>
+      </footer>
+
+      {
+        showModal ? (
+          <div className="add-todo-modal">
+            <form onSubmit={addTodo}>
+              <img onClick={() => setShowModal(false)} src={closeIcon} className="close-icon"></img>
+              <h2>Add Todo</h2>
+              <input value={todoTitle} onChange={e => setTodoTitle(e.target.value)} placeholder="Add todo..." tabIndex="1" className="add-todo-input" type="text"></input>
+              <input tabIndex="2" type="submit"></input>
+            </form>
+            
+          </div>
+        ) : null
+      }
     </div>
-  );
+  )
 }
 
 export default App;
